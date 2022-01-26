70f0ff6b71655d5d4b5417b7491d0e1cbcb997c7
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-01-26 13:52:52 -0300

    n

--- cogs/event.py
+++ cogs/event.py
@@ -4,7 +4,7 @@ import main
 from cogs.config import Config
 from discord.ext import commands
 
-class Events(commands.Cogs):
+class Events(commands.Cog):
     def __init__(self, client):
         self.client = client
 
