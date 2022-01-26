e81e1ff78c3e3a645caeeef2ddd51257d59e9573
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-01-26 19:47:56 -0300

    t

--- cogs/music.py
+++ cogs/music.py
@@ -152,7 +152,7 @@ class Music(commands.Cog):
         """Skip this song."""
         if ctx.author.voice:
             embed = discord.Embed(color=Config.botcolor(),title=f"Skipping.")
-            voice = discord.uitls.get(ctx.bot.voice_clients, guild = ctx.guild)
+            voice = discord.utils.get(ctx.bot.voice_clients, guild = ctx.guild)
             voice.stop()
             await self.check_queue(ctx)
             await ctx.send(embed=embed, delete_after=5)
