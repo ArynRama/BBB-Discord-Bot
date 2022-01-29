543a93959881b8059b830900c7e4e4bcfe268a98
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-01-29 16:06:16 -0300

    create owner.py

---
+++ cogs/owner.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class Owner(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def disable(self, name):
+        for a in self.client.commands:
+            if name in a.qualified_name:
+                a.update(enable=False)
+    
+    @commands.command()
+    async def enable(self, name):
+        for a in self.client.commands:
+            if name in a.qualified_name:
+                a.update(enable=True)
+        
+def setup(client):
+    client.add_cog(Owner(client))
