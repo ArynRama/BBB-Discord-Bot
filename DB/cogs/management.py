10d2157bf111b0b0eec50428b920499f9699c403
arynrama <aryanrama26@gmail.com>
2022-06-25 14:24:59 -0300

    cxx

---
+++ DB/cogs/management.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+from DB.essential.config import botcolor
+from ..essential.checks import is_dev
+
+class Management(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @is_dev
+    async def whois(self, ctx, args):
+        lookup = self.client.fetch_user(args)
+        embed = discord.Embed(title=lookup,color=botcolor())
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Management(client))
