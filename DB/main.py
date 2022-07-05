29fc8cf77636219c5143d17c4f133c544ee349da
arynrama <aryanrama26@gmail.com>
2022-07-05 19:54:12 -0300

    hj

--- DB/main.py
+++ DB/main.py
@@ -2,8 +2,10 @@ import os
 import dotenv
 import discord
 import pyrebase
+import threading
 import tracemalloc
 from discord import client
+from time import sleep, time
 from discord.ext import commands
 from essential.config import prefix
 from essential.database import datab, firebases, authen, authentic
@@ -25,11 +27,32 @@ class clients(commands.Bot):
         print('Owner: ArynRama#6043')
         print('------')
 
-    firebase = firebases
-    authentication = authentic
-    auth = authen
-    db = datab
-    
+    firebaseConfig = {
+        "apiKey": "AIzaSyB1ujCtmCOnd7aMft806lJguZV3gXVt6l0",
+        "authDomain": "discord-bot-database-b44ec.firebaseapp.com",
+        "databaseURL": "https://discord-bot-database-b44ec-default-rtdb.firebaseio.com",
+        "projectId": "discord-bot-database-b44ec",
+        "storageBucket": "discord-bot-database-b44ec.appspot.com",
+        "messagingSenderId": "769471483502",
+        "appId": "1:769471483502:web:579519c2b0e9b39197a07f",
+        "measurementId": "G-RPC6GZKFNR"
+    }
+
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    authentication = firebases.auth()
+    auth = authentic.sign_in_with_email_and_password(os.getenv("FB_Email"), os.getenv("FB_Pass"))
+    db = firebases.database()
+    idToken = authen['idToken']
+
+    def reload():
+        while True:
+            sleep(60 - time() % 60)
+            authentic.refresh(authen['refreshToken'])
+            clients.idToken = authen['idToken']
+
+    thread = threading.Thread(target=reload)
+    thread.start()
+
 client = clients(command_prefix=prefix(),description=description, intent=intents, help_command=HelpCmd())
 
 class LoadCogs:
